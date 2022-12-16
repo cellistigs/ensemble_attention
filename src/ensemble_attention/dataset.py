b50615ef525a09455d7d0081cc5612eb6298771b
Ubuntu <ubuntu@ip-172-31-76-55.ec2.internal>
2022-12-16 23:19:19 +0000

    added data set and moduel

--- src/ensemble_attention/dataset.py
+++ src/ensemble_attention/dataset.py
@@ -5,17 +5,19 @@ from PIL import Image
 import numpy as np 
 import pytorch_lightning as pl
 import requests
-from torch.utils.data import Dataset,Dataloader
+from torch.utils.data import Dataset,DataLoader
 import torch
-from tqdm import dqdm 
+from tqdm import tqdm 
 import pandas as pd
 
-def WineDataset(Dataset):
-    def __init__(self,root_dir,transform =None,target_transform = None,seed = None,color = None):
+class WineDataset(Dataset):
+    def __init__(self,root_dir,transform =None,target_transform = None,seed = 0,test_size = 1000,color = None,train = False):
+        if seed is not None:
+            np.random.seed(seed)
         #super().__init__(root_dir,transform = transform,target_transform = target_transform)
         self.root_dir = root_dir
-        self.raw_data_red = pd.read_csv(os.path.join(root_dir,"winequality-red.csv"),sep = ";").values()
-        self.raw_data_white = pd.read_csv(os.path.join(root_dir,"winequality-white.csv"),sep = ";").values()
+        self.raw_data_red = pd.read_csv(os.path.join(root_dir,"winequality-red.csv"),sep = ";").values
+        self.raw_data_white = pd.read_csv(os.path.join(root_dir,"winequality-white.csv"),sep = ";").values
         self.transform = transform
         self.target_transform = target_transform
         if color == None:
@@ -24,21 +26,45 @@ def WineDataset(Dataset):
             self.raw_data = self.raw_data_red
         elif color == "white":    
             self.raw_data = self.raw_data_white
-        self.features = self.raw_data[:,:-1]    
-        self.targets = self.raw_data[:,-1]
+        indices = np.random.permutation(range(len(self.raw_data)))    
+        test_indices = indices[:test_size]
+        train_indices = indices[test_size:]
+        if train == False:
+            self.features = self.raw_data[test_indices,:-1]    
+            self.targets = self.raw_data[test_indices,-1]
+        else:    
+            self.features = self.raw_data[train_indices,:-1]    
+            self.targets = self.raw_data[train_indices,-1]
         
     def __len__(self):
         return len(self.raw_data)
 
     def __getitem__(self,idx):
-        feature,target = self.features[i],self.targets[i]
+        feature,target = self.features[idx],self.targets[idx]
         if self.transform:
             feature = self.transform(feature)
         if self.target_transform:    
             target = self.target_transform(target)
         return feature,target    
 
-        
-        
-
+class WineDataModule(pl.LightningDataModule):
+    def __init__(self,args):
+        super().__init__()
+        self.hparams = args
+        self.wine_predict = WineDataset(self.hparams.data_dir,train = False)
+        self.wine_train = WineDataset(self.hparams.data_dir,train = True)
+    def train_dataloader(self):
+        return DataLoader(self.wine_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            drop_last=False,
+            pin_memory=True,)
+    def val_dataloader(self):
+        return DataLoader(self.wine_predict,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            drop_last=False,
+            pin_memory=True,)
+    def test_dataloader(self):
+        return self.val_dataloader()
 
