b2c4dc54c0c00eca225a7b1a079018cc28ec14a2
Ubuntu <ubuntu@ip-172-31-76-55.ec2.internal>
2022-12-20 20:38:15 +0000

    added regression-as-classification and it works

--- src/ensemble_attention/dataset.py
+++ src/ensemble_attention/dataset.py
@@ -7,6 +7,7 @@ import pytorch_lightning as pl
 import requests
 from torch.utils.data import Dataset,DataLoader
 import torch
+from torchvision.transforms import ToTensor
 from torchvision.datasets import MNIST
 from tqdm import tqdm 
 import pandas as pd
@@ -79,15 +80,15 @@ class WineDataModule(pl.LightningDataModule):
 class OneHotTransform():
     def __init__(self,num_classes):
         self.num_classes = num_classes
-    def __class__(self,data):    
-        return torch.nn.functional.one_hot(data,self.num_classes)
+    def __call__(self,data):    
+        return torch.nn.functional.one_hot(torch.tensor(data),self.num_classes).float()
 
 class MNISTModule(pl.LightningDataModule):
     def __init__(self,args):
         super().__init__()
         self.hparams = args
-        self.mnist_predict = MNIST(self.hparams.data_dir,train = False,transform = None,target_transform =OneHotTransform)
-        self.mnist_train = MNIST(self.hparams.data_dir,train = True,transform = None,target_transform = OneHotTransform)
+        self.mnist_predict = MNIST(self.hparams.data_dir,train = False,transform=ToTensor(),target_transform=OneHotTransform(num_classes=10),download = True)
+        self.mnist_train = MNIST(self.hparams.data_dir,train = True,transform =ToTensor(),target_transform=OneHotTransform(num_classes=10),download =True)
     def train_dataloader(self,shuffle = False,aug = False):
         return DataLoader(self.mnist_train,
             batch_size=self.hparams.batch_size,
