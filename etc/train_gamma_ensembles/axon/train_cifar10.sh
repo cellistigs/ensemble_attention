2e02fce48df5749bd8af89da557f90a9f606a98a
ekellbuch <ekellbuchanan@gmail.com>
2022-12-20 14:42:03 -0500

    update default seed values

--- etc/train_gamma_ensembles/axon/train_cifar10.sh
+++ etc/train_gamma_ensembles/axon/train_cifar10.sh
@@ -12,12 +12,16 @@
 #SBATCH -c 4
 #SBATCH --mail-type=ALL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=ekb2154@columbia.edu         # Where to send mail (e.g. uni123@columbia.edu)
-. activate interp
 
+ml load anaconda3-2019.03;
+source activate interp
+#. activate interp
 
 echo "Begin call: scrips/run.py $1"
-pushd ../../../
-python  scripts/run.py $1
+#pushd ../../../
+#python  scripts/run.py $1
+python /home/ekb2154/data/libraries/ensemble_attention/scripts/run.py $1
 echo "Ran scrips/run.py $1"
 
-popd
+source deactivate
+#popd
