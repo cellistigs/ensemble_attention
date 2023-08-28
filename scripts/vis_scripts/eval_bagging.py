9c34f2811ae536937c12a45fc27772de39ef5ae6
Ubuntu <ubuntu@ip-172-31-71-50.ec2.internal>
2023-08-28 17:49:14 +0000

    added bagging evaluation error bars.

--- scripts/vis_scripts/eval_bagging.py
+++ scripts/vis_scripts/eval_bagging.py
@@ -20,7 +20,7 @@ standard_resnet8s = [
         "2023-03-30/15-33-55",
         "2023-03-30/20-34-39", 
         "2023-03-30/20-44-13",
-        #"2023-03-30/20-53-48",
+        "2023-03-30/20-53-48",
         ]
 bagged_resnet8s = [
         "2023-03-29/22-31-52", 
@@ -33,7 +33,7 @@ bagged_resnet8s = [
         "2023-03-30/16-13-12",
         "2023-03-30/21-13-10",  
         "2023-03-30/21-22-54",
-        #"2023-03-30/21-32-37",
+        "2023-03-30/21-32-37",
         ]
 
 def extract_preds_labels(s,condition = "ind"):
@@ -60,6 +60,14 @@ def ens_evaluate(preds,labels):
     ens_acc = len(np.where(np.argmax(ens_preds,axis = 1)==labels)[0])/float(len(labels))
     return ens_acc
 
+def bootstrap_ens_evaluate(preds,labels,members = 4,samples = 100):
+    preds_subsamples = [np.array(preds)[np.random.choice(len(preds),4,replace =False).astype(int)] for i in range(samples)]
+    ###### fix to iterate over. 
+    ens_acc = []
+    for preds in preds_subsamples:
+        ens_preds = np.mean(preds,axis = 0)
+        ens_acc.append(len(np.where(np.argmax(ens_preds,axis = 1)==labels)[0])/float(len(labels)))
+    return ens_acc
 
 
 
@@ -70,9 +78,10 @@ def main():
         for si,s in enumerate([standard_resnet8s,bagged_resnet8s]):
             preds,labels = extract_preds_labels(s,condition)
             acc = evaluate(preds,labels)
-            ens_acc = ens_evaluate(preds,labels)
+            ens_acc = bootstrap_ens_evaluate(preds,labels)
             ax[ci].plot([si for a in acc],acc,"o",color = "blue",label = "indiv")
-            ax[ci].plot(si,ens_acc,"x",color = "red",label = "ens")
+            #ax[ci].plot([si for a in ens_acc],ens_acc,"x",color = "red",label = "ens")
+            ax[ci].errorbar(si,np.mean(ens_acc),yerr=np.std(ens_acc),marker="x",color = "red",label = "ens")
             ax[ci].set_xticks([0,1])
             ax[ci].set_xticklabels(["Standard\n Ensembling","Bagged\n Ensembles"])
             ax[ci].set_xlim([-0.25,1.25])
@@ -80,10 +89,6 @@ def main():
         plt.legend()
     plt.savefig("eval_bagging.png")    
         
-        
-        
-
-
 
 if __name__ == "__main__":    
     main()
